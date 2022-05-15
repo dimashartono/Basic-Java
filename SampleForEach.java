bc2b939c9ef36ff9aaca17ff92707458e6bd84a7
dimashartono <hartono.dimas29@gmail.com>
2022-05-16 05:02:51 +0700

    Using ForEach in Java

---
+++ SampleForEach.java
@@ -0,0 +1,16 @@
+public class SampleForEach {
+
+    public static void main(String[] args) {
+
+        String[] villages = {
+                "Gabus", "Wetan", "Saradan", "Kemped"
+        };
+
+        for(var village : villages){
+            System.out.println(village);
+        }
+
+
+    }
+
+}
